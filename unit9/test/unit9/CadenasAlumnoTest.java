8c9ade31f399cb439f994927d87f67925aac9072
JMDura <josemii1993@gmail.com>
2015-05-19 14:44:53 +0200

    Añadidas pruebas para la mayoria de metodos

--- unit9/test/unit9/CadenasAlumnoTest.java
+++ unit9/test/unit9/CadenasAlumnoTest.java
@@ -50,53 +50,78 @@ public class CadenasAlumnoTest {
     @Test
     public void testSonIguales() {
         System.out.println("sonIguales");
-        String cadena1 = "";
-        String cadena2 = "";
+        String cadena1 = "hola";
+        String cadena2 = "hola";
         CadenasAlumno instance = new CadenasAlumno();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.sonIguales(cadena1, cadena2);
+        assertEquals(expResult, result);
+        
+        expResult=false;
+        cadena1="hola";
+        cadena2="h";
+        result = instance.sonIguales(cadena1, cadena2);
         assertEquals(expResult, result);
     }
 
     @Test
     public void testEsMayor() {
         System.out.println("esMayor");
-        String cadena1 = "";
+        String cadena1 = "asdas";
         String cadena2 = "";
         CadenasAlumno instance = new CadenasAlumno();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.esMayor(cadena1, cadena2);
+        assertEquals(expResult, result);
+        
+        cadena1="";
+        cadena2="asdada";
+        expResult=false;
+        result = instance.esMayor(cadena1, cadena2);
         assertEquals(expResult, result);
     }
 
     @Test
     public void testEsMenor() {
         System.out.println("esMenor");
-        String cadena1 = "";
+        String cadena1 = "asdas";
         String cadena2 = "";
         CadenasAlumno instance = new CadenasAlumno();
         boolean expResult = false;
         boolean result = instance.esMenor(cadena1, cadena2);
+        assertEquals(expResult, result);
+        
+        cadena1="";
+        cadena2="asdada";
+        expResult=true;
+        result = instance.esMenor(cadena1, cadena2);
         assertEquals(expResult, result);
     }
 
     @Test
     public void testComparaIgnorandoMayusculas() {
         System.out.println("comparaIgnorandoMayusculas");
-        String cadena1 = "";
-        String cadena2 = "";
+        String cadena1 = "hola";
+        String cadena2 = "HOLA";
         CadenasAlumno instance = new CadenasAlumno();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.comparaIgnorandoMayusculas(cadena1, cadena2);
+        assertEquals(expResult, result);
+        
+        
+        cadena1="Hola";
+        cadena2="ola";
+        expResult = false;
+        result = instance.comparaIgnorandoMayusculas(cadena1, cadena2);
         assertEquals(expResult, result);
     }
 
     @Test
     public void testConvertirMayusculas() {
         System.out.println("convertirMayusculas");
-        String cadena = "";
+        String cadena = "hola";
         CadenasAlumno instance = new CadenasAlumno();
-        String expResult = "";
+        String expResult = "HOLA";
         String result = instance.convertirMayusculas(cadena);
         assertEquals(expResult, result);
     }
@@ -104,9 +129,9 @@ public class CadenasAlumnoTest {
     @Test
     public void testConvertirMinusculas() {
         System.out.println("convertirMinusculas");
-        String cadena = "";
+        String cadena = "HOLA";
         CadenasAlumno instance = new CadenasAlumno();
-        String expResult = "";
+        String expResult = "hola";
         String result = instance.convertirMinusculas(cadena);
         assertEquals(expResult, result);
     }
@@ -114,11 +139,11 @@ public class CadenasAlumnoTest {
     @Test
     public void testReemplazaPrimero() {
         System.out.println("reemplazaPrimero");
-        String cadena = "";
-        String regla = "";
-        String cambio = "";
+        String cadena = "hola";
+        String regla = "h";
+        String cambio = "a";
         CadenasAlumno instance = new CadenasAlumno();
-        String expResult = "";
+        String expResult = "aola";
         String result = instance.reemplazaPrimero(cadena, regla, cambio);
         assertEquals(expResult, result);
     }
@@ -126,8 +151,8 @@ public class CadenasAlumnoTest {
     @Test
     public void testPosicionPrimeraCadena() {
         System.out.println("posicionPrimeraCadena");
-        String cadena = "";
-        String letra = "";
+        String cadena = "hola";
+        String letra = "h";
         CadenasAlumno instance = new CadenasAlumno();
         long expResult = 0L;
         long result = instance.posicionPrimeraCadena(cadena, letra);
@@ -172,9 +197,9 @@ public class CadenasAlumnoTest {
     @Test
     public void testQuitaEspacios() {
         System.out.println("quitaEspacios");
-        String cadena = "";
+        String cadena = "            ey       ";
         CadenasAlumno instance = new CadenasAlumno();
-        String expResult = "";
+        String expResult = "ey";
         String result = instance.quitaEspacios(cadena);
         assertEquals(expResult, result);
     }
@@ -182,10 +207,10 @@ public class CadenasAlumnoTest {
     @Test
     public void testEmpiezaCon() {
         System.out.println("empiezaCon");
-        String cadena = "";
-        String prefijo = "";
+        String cadena = "hola";
+        String prefijo = "h";
         CadenasAlumno instance = new CadenasAlumno();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.empiezaCon(cadena, prefijo);
         assertEquals(expResult, result);
     }
@@ -193,10 +218,10 @@ public class CadenasAlumnoTest {
     @Test
     public void testAcabaEn() {
         System.out.println("acabaEn");
-        String cadena = "";
-        String prefijo = "";
+        String cadena = "EN";
+        String prefijo = "N";
         CadenasAlumno instance = new CadenasAlumno();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.acabaEn(cadena, prefijo);
         assertEquals(expResult, result);
     }
@@ -215,8 +240,8 @@ public class CadenasAlumnoTest {
     @Test
     public void testMuestraSplit() {
         System.out.println("muestraSplit");
-        String cadena = "";
-        String regla = "";
+        String cadena = "hola que tal";
+        String regla = " ";
         CadenasAlumno instance = new CadenasAlumno();
         instance.muestraSplit(cadena, regla);
     }
